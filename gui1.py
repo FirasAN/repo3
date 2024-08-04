8bf6b5b2c8f40eb94d6c292c0485e010bb03ffbd
firasAN <firas.abdelnour@gmail.com>
2024-08-04 20:06:16 +0300

    Initial commit2

--- gui1.py
+++
@@ -1,9 +0,0 @@
-# This program displays an empty window.
-from tkinter import *
-def main():
-    # Create the main window widget.
-    main_window = Tk()
-    # Enter the tkinter main loop.
-    mainloop()
-# Call the main function.
-main()
